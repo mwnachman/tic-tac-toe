5955835f907454d35ba92299285bc74ec20166cc
Meredith Nachman <mwnachman@gmail.com>
2017-02-01 16:09:51 -0800

    fixed bug in end state

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -7,14 +7,28 @@ const GameReducer = (state =
     let player = action.payload.player;
     let square = action.payload.square;
     let stateBeforeSquare = state.slice(0, square);
+    console.log('statebeforesquare select', stateBeforeSquare);
     let stateAfterSquare = state.slice(square + 1); 
+    console.log('stateAfterSquare select', stateAfterSquare);
     return stateBeforeSquare.concat(player).concat(stateAfterSquare);
   } else if ( action.type === 'COMPUTER_MOVE' ) {
-    let player = action.payload.player;
-    let square = action.payload.move;
-    let stateBeforeSquare = state.slice(0, square);
-    let stateAfterSquare = state.slice(square + 1); 
-    return stateBeforeSquare.concat(player).concat(stateAfterSquare);
+    var boardFull = true;
+    for (let i = 0; i < 9; i++) {
+      if (state[i] === null) {
+        boardFull = false;
+      }
+    }
+    if (boardFull === true) {
+      return state;
+    } else {
+      let player = action.payload.player;
+      let square = action.payload.move;
+      let stateBeforeSquare = state.slice(0, square);
+      console.log('statebeforesquare comp', stateBeforeSquare);
+      let stateAfterSquare = state.slice(square + 1); 
+      console.log('stateAfterSquare comp', stateAfterSquare);
+      return stateBeforeSquare.concat(player).concat(stateAfterSquare);
+    }
   } else if ( action.type === 'NEW_GAME') {
     return originalGameState;
   } else {
